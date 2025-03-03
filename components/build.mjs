77df12ffd6f53e1332a354d8fa65898d8bd82c33
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2025-03-03 18:34:29 +0100

    add banner

--- components/build.mjs
+++ components/build.mjs
@@ -1,8 +1,20 @@
 import { buildSync } from "esbuild";
 
+const banner = `
+// __     _______
+// \ \   / /_   _|
+//  \ \ / /  | |
+//   \ V /   | |
+//    \_/    |_|
+`;
+
 buildSync({
   entryPoints: ["./index.ts"],
   outfile: "../public/js/components.js",
+
+  banner: {
+    js: banner,
+  },
 
   minify: true,
   external: [],
