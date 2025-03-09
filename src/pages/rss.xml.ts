98f355c4757bfbc27ba13cafc01b14e0aaa3f9a6
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2025-03-09 02:13:51 +0100

    better rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,6 +3,8 @@ import { getCollection, z } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 import type { APIRoute } from "astro";
 
+export const prerender = true;
+
 export const GET: APIRoute = async (context) => {
   const posts = (await getCollection("article")).filter(
     (post) => !!post.data.pubDate,
@@ -11,9 +13,16 @@ export const GET: APIRoute = async (context) => {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: z.instanceof(URL).parse(context.site),
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/logs/${post.id}/`,
-    })),
+    items: posts.map((post) => {
+      let title = post.id.replaceAll("-", " ");
+      title = title.charAt(0).toUpperCase() + title.slice(1);
+      return {
+        pubDate: post.data.pubDate,
+        categories: post.data.tags,
+        description: post.data.description,
+        title,
+        link: `/logs/${post.id}/`,
+      };
+    }),
   });
 };
