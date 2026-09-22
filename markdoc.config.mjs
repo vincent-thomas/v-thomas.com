be78b69f449e47ec63a16f230d7473706603a840
Vincent Thomas <vincent@v-thomas.com>
2026-09-22 14:44:49 +0200

    Open external Markdoc links in new tabs

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,6 +1,32 @@
-import { defineMarkdocConfig, component } from '@astrojs/markdoc/config';
+import { Markdoc, component, defineMarkdocConfig, nodes } from '@astrojs/markdoc/config';
+
+function isExternalHttpUrl(href) {
+  if (typeof href !== 'string' || !/^https?:\/\//i.test(href)) return false;
+
+  try {
+    const url = new URL(href);
+    return (url.protocol === 'http:' || url.protocol === 'https:') && Boolean(url.hostname);
+  } catch {
+    return false;
+  }
+}
 
 export default defineMarkdocConfig({
+  nodes: {
+    link: {
+      ...nodes.link,
+      transform(node, config) {
+        const attributes = node.transformAttributes(config);
+
+        if (isExternalHttpUrl(attributes.href)) {
+          attributes.target = '_blank';
+          attributes.rel = 'noopener noreferrer';
+        }
+
+        return new Markdoc.Tag('a', attributes, node.transformChildren(config));
+      },
+    },
+  },
   tags: {
     nav: {
       render: 'nav',
